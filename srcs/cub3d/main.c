3517e2110e97fcf692a5361fbd45e780acb882a4
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-04-22 13:35:51 -0300

    ok

--- srcs/cub3d/main.c
+++ srcs/cub3d/main.c
@@ -6,7 +6,7 @@
 /*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 18:29:29 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/04/22 12:02:20 by jonas            ###   ########.fr       */
+/*   Updated: 2025/04/22 13:35:01 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,9 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		return (ft_putendl_fd_1("How to use: ./cub3d <map.cub>", 2));
 	if (!check_cub(argv[1]))
-		return (ft_putendl_fd_1("Directory/Empty file/.cub Error", 2));
+		return (1);
 	if (!init_game(argv[1], &game))
-	{
 		destroy(&game);
-		return (ft_putendl_fd_1("Init Error", 2));
-	}
 	show_textures(&game);
 	run_window(&game);
 	return (destroy(&game));
