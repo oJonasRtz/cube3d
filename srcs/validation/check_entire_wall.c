c4f9b85050ee70b4304b58d836f3e7fb45c1be0c
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-04-22 11:48:37 -0300

    ok

--- srcs/validation/check_entire_wall.c
+++ srcs/validation/check_entire_wall.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 16:05:55 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/04/20 17:33:51 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/04/22 11:45:47 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,11 @@ int	check_entire_wall(t_game *game, int index)
 	int	count;
 
 	count = 0;
-	while (game->true_game_map[index][count] == ' ')
-		count++;
 	while (game->true_game_map[index][count] != '\n'
 		&& game->true_game_map[index][count] != '\0')
 	{
-		if (game->true_game_map[index][count] != '1')
+		if (game->true_game_map[index][count] != '1'
+			&& game->true_game_map[index][count] != ' ')
 			return (0);
 		count++;
 	}
