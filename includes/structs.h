a6246eaacb5cb6e4914b37e9fdad2b06cc8911a4
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-04-23 14:26:54 -0300

    ok

--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 12:55:55 by jopereir          #+#    #+#             */
-/*   Updated: 2025/04/23 13:04:53 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/04/23 13:23:24 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,8 @@ typedef struct s_game
 	int		invalid_map;
 	int		player_x;
 	int		player_y;
+	int		offset_x;
+	int		offset_y;
 	char	**maps;
 	int		no_texture_index;
 	int		so_texture_index;
--- srcs/set_window/run_window.c
+++ srcs/set_window/run_window.c
@@ -6,7 +6,7 @@
 /*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:21:48 by jopereir          #+#    #+#             */
-/*   Updated: 2025/04/23 11:38:50 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/04/23 14:24:07 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,24 @@ static int	handle_no_event(void)
 
 static int	handle_input(int key, t_game *game)
 {
+	int	max_offset_x;
+	int	max_offset_y;
+
+	max_offset_x = (game->width * TILE_SIZE) - game->width;
+	max_offset_y = (game->heigth * TILE_SIZE) - game->heigth;
 	if (key == 65307)
 		destroy(game);
+	if (key == 65361)
+		game->offset_x -= TILE_SIZE;
+	if (key == 65362)
+		game->offset_y -= TILE_SIZE;
+	if (key == 65363)
+		game->offset_x += TILE_SIZE;
+	if (key == 65364)
+		game->offset_y += TILE_SIZE;
+	if (game->offset_x > max_offset_x || game->offset_y > max_offset_y)
+		return (1);
+	minimap(game);
 	return (0);
 }
 
@@ -31,6 +47,7 @@ void	run_window(t_game *game)
 	mlx_loop_hook(game->mlx.mlx_ptr, &handle_no_event, NULL);
 	mlx_key_hook(game->mlx.win, &handle_input, game);
 	mlx_hook(game->mlx.win, 17, 0, &destroy, game);
+	mlx_hook(game->mlx.win, 2, 1L << 0, &handle_input, game);
 	minimap(game);
 	mlx_loop(game->mlx.mlx_ptr);
 }
