ba6e587d6c661829864d9202d33918cf19868016
Jonas Alberto Pereira <jopereir@c1r1p1.42sp.org.br>
2025-04-23 15:52:14 -0300

    a

--- srcs/set_window/run_window.c
+++ srcs/set_window/run_window.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   run_window.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:21:48 by jopereir          #+#    #+#             */
-/*   Updated: 2025/04/23 15:18:27 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/04/23 15:51:50 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,18 @@ static int	handle_input(int key, t_game *game)
 {
 	if (key == 65307)
 		destroy(game);
-	if (key == 65361)
-		game->offset_x -= TILE_SIZE;
-	if (key == 65362)
-		game->offset_y -= TILE_SIZE;
-	if (key == 65363)
-		game->offset_x += TILE_SIZE;
-	if (key == 65364)
-		game->offset_y += TILE_SIZE;
-	mlx_clear_window(game->mlx.mlx_ptr, game->mlx.win);
-	game->min_row = game->offset_y / TILE_SIZE;
-	game->min_col = game->offset_x / TILE_SIZE;
-	minimap(game);
+	// if (key == 65361)
+	// 	game->offset_x -= TILE_SIZE;
+	// if (key == 65362)
+	// 	game->offset_y -= TILE_SIZE;
+	// if (key == 65363)
+	// 	game->offset_x += TILE_SIZE;
+	// if (key == 65364)
+	// 	game->offset_y += TILE_SIZE;
+	// mlx_clear_window(game->mlx.mlx_ptr, game->mlx.win);
+	// game->min_row = game->offset_y / TILE_SIZE;
+	// game->min_col = game->offset_x / TILE_SIZE;
+	// minimap(game);
 	return (0);
 }
 
@@ -44,6 +44,6 @@ void	run_window(t_game *game)
 	mlx_key_hook(game->mlx.win, &handle_input, game);
 	mlx_hook(game->mlx.win, 17, 0, &destroy, game);
 	mlx_hook(game->mlx.win, 2, 1L << 0, &handle_input, game);
-	minimap(game);
+	// minimap(game);
 	mlx_loop(game->mlx.mlx_ptr);
 }
