c3f16f7ae5b38129496270a74d78d4b79bedd827
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-04-24 20:05:57 -0300

    ok

--- srcs/bonus/draw_radius.c
+++ srcs/bonus/draw_radius.c
@@ -3,26 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   draw_radius.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
+/*   By: fruan-ba <fruan-ba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 11:03:01 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/04/24 17:26:51 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/04/24 20:05:01 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
+static void	new_position_px_py(t_game *game, double *px, double *py)
+{
+	*px = (game->width / 2) + game->offset_x;
+	*py = (game->heigth / 2) + game->offset_y;
+}
+
 void	draw_radius(t_game *game)
 {
-	int	index;
+	int		index;
 	double	px;
-	int	map_x;
-	int	map_y;
+	int		map_x;
+	int		map_y;
 	double	py;
 
 	index = 0;
-	px = (game->width / 2) + game->offset_x;
-	py = (game->heigth / 2) + game->offset_y;
+	new_position_px_py(game, &px, &py);
 	while (index < 1000)
 	{
 		map_x = px / TILE_SIZE;
@@ -31,7 +36,8 @@ void	draw_radius(t_game *game)
 			|| map_y >= game->heigth / TILE_SIZE
 			|| map_x >= (int)ft_strlen(game->true_game_map[map_y]))
 			break ;
-		if (game->true_game_map[map_y][map_x] == '1')
+		if (game->true_game_map[map_y][map_x] == '1'
+			|| game->true_game_map[map_y][map_x] == ' ')
 			break ;
 		mlx_pixel_put(game->mlx.mlx_ptr, game->mlx.win, px - game->offset_x,
 			py - game->offset_y, COLOUR_DEFAULT);
