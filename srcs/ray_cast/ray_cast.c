62b860c3ae89a37d494a90488236edf8a7a122f9
Jonas <jonasper19@gmail.com>
2025-04-24 18:41:21 -0300

    a

--- srcs/ray_cast/ray_cast.c
+++ srcs/ray_cast/ray_cast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ray_cast.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 13:01:38 by jopereir          #+#    #+#             */
-/*   Updated: 2025/04/23 15:48:01 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/04/24 11:05:56 by jonas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 
 void	ray_cast(t_game *game)
 {
-	/*
-		draw the ray
-	*/
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+
+	dir_x = -1;
+	dir_y = 0;
+	plane_x = 0;
+	plane_y = 0.66;
+	while(1)
+	{
+		
+	}
 }
