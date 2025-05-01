878771a87d6e13f7511195b245d4a73c3787cd7a
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-05-01 12:29:01 -0300

    ok

--- the_power_3d_dda/includes/dda.h
+++ the_power_3d_dda/includes/dda.h
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 11:36:33 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/04/30 18:51:20 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/05/01 12:26:24 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	init_camera_plane(t_game *game);
 void	init_ray(t_game *game, int x);
 void	init_dda_params(t_game *game);
 void	dda_loop(t_game *game);
-double	calculate_perp_dist_update_wall(t_game *game);
+void	calculate_perp_dist_update_wall(t_game *game);
 void	update_offset(t_game *game);
 
 #endif /* DDA_H */
--- the_power_3d_dda/srcs/dda/calculate_perp_dist_update_wall.c
+++ the_power_3d_dda/srcs/dda/calculate_perp_dist_update_wall.c
@@ -6,13 +6,13 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 16:47:44 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/04/30 16:58:49 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/05/01 12:27:35 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-double	calculate_perp_dist_update_wall(t_game *game)
+void	calculate_perp_dist_update_wall(t_game *game)
 {
 	double	px;
 	double	py;
@@ -30,6 +30,8 @@ double	calculate_perp_dist_update_wall(t_game *game)
 		perpDist = ((game->dda.mapY - py) + (1 - game->dda.stepY) / 2.0)
 			/ game->dda.rayDirY;
 	}
+	if (perpDist <= 0.0)
+		perpDist = 0.0001;
 	game->dda.lineHeight = (int)(game->heigth / perpDist);
 	game->dda.drawStart = -game->dda.lineHeight / 2 + game->heigth / 2;
 	if (game->dda.drawStart < 0)
@@ -37,5 +39,4 @@ double	calculate_perp_dist_update_wall(t_game *game)
 	game->dda.drawEnd = game->dda.lineHeight / 2 + game->heigth / 2;
 	if (game->dda.drawEnd >= game->heigth)
 		game->dda.drawEnd = game->heigth - 1;
-	return (perpDist);
 }
