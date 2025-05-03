ac8e14b14294de8f032d81c2c84ddeb4473d0fb0
Fernando Ruan <fernandosingaporefuture@gmail.com>
2025-05-02 21:04:41 -0300

    colisions are done

--- CUB3D_OFFICIAL/srcs/ray_cast/can_move.c
+++ CUB3D_OFFICIAL/srcs/ray_cast/can_move.c
@@ -12,13 +12,8 @@
 
 #include "../../includes/cub3d.h"
 
-int	can_move(t_game *game, double next_x, double next_y)
+static int	security(t_game *game, int map_x, int map_y)
 {
-	int	map_x;
-	int	map_y;
-
-	map_x = (int)(next_x + (game->width / 2)) / TILE_SIZE;
-	map_y = (int)(next_y + (game->heigth / 2)) / TILE_SIZE;
 	if (map_x < 0 || map_x >= game->width_map || map_y < 0
 		|| map_y >= game->heigth_map)
 		return (0);
@@ -28,3 +23,30 @@ int	can_move(t_game *game, double next_x, double next_y)
 	return (1);
 }
 
+int	can_move(t_game *game, double next_x, double next_y)
+{
+	double	margin;
+	int		map_x;
+	int		map_y;
+	int		check_x;
+	int		check_y;
+
+	margin = TILE_SIZE * 0.2;
+	check_y = -1;
+	while (check_y <= 1)
+	{
+		check_x = -1;
+		while (check_x <= 1)
+		{
+			map_x = (int)(next_x + check_x * margin
+					+ (game->width / 2)) / TILE_SIZE;
+			map_y = (int)(next_y + check_y * margin
+					+ (game->heigth / 2)) / TILE_SIZE;
+			if (!security(game, map_x, map_y))
+				return (0);
+			check_x++;
+		}
+		check_y++;
+	}
+	return (1);
+}
