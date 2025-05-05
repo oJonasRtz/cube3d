09c3e7a792d7c14eeb26793f5d3e7be3e6ed8a5f
Jonas <jonasper19@gmail.com>
2025-05-05 17:23:14 -0300

    distorcao horizontal

--- CUB3D_OFFICIAL/srcs/the_cub_3d_powered/get_tex_colour.c
+++ CUB3D_OFFICIAL/srcs/the_cub_3d_powered/get_tex_colour.c
@@ -6,24 +6,33 @@
 /*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 15:19:35 by jonas             #+#    #+#             */
-/*   Updated: 2025/05/05 17:14:14 by jonas            ###   ########.fr       */
+/*   Updated: 2025/05/05 17:22:46 by jonas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static double	get_perpwalldist(t_dda *dda)
+{
+	if (dda->side == 0)
+		return ((dda->mapx - dda->px + (1 - dda->stepx) / 2) / dda->raydirx);
+	return ((dda->mapy - dda->py + (1 - dda->stepy) / 2) / dda->raydiry);
+}
+
 static int	get_tex_x(t_dda *dda)
 {
 	int		tex_x;
 	double	wall_x;
+	double	perpwalldist;
 
+	perpwalldist = get_perpwalldist(dda);
 	if (dda->side == 0)
-		wall_x = dda->py + dda->perpwalldist * dda->raydiry;
+		wall_x = dda->py + perpwalldist * dda->raydiry;
 	else
-		wall_x = dda->px + dda->perpwalldist * dda->raydirx;
+		wall_x = dda->px + perpwalldist * dda->raydirx;
 	wall_x -= floor(wall_x);
 	tex_x = (int)(wall_x * TILE_SIZE);
-	if ((dda->side == 0 && dda->raydirx > 0) || (dda->side == 1 && dda->raydirx < 0))
+	if ((dda->side == 0 && dda->raydirx > 0) || (dda->side == 1 && dda->raydiry < 0))
 		tex_x = TILE_SIZE - tex_x - 1;
 	return (tex_x);
 }
