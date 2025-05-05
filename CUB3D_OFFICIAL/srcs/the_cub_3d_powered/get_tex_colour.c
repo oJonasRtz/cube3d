ef001e676a5b811cbb4f6194bb479eed670d5da2
Jonas <jonasper19@gmail.com>
2025-05-05 15:30:04 -0300

    norminette

--- CUB3D_OFFICIAL/srcs/the_cub_3d_powered/get_tex_colour.c
+++ CUB3D_OFFICIAL/srcs/the_cub_3d_powered/get_tex_colour.c
@@ -6,30 +6,41 @@
 /*   By: jonas <jonas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 15:19:35 by jonas             #+#    #+#             */
-/*   Updated: 2025/05/05 15:21:17 by jonas            ###   ########.fr       */
+/*   Updated: 2025/05/05 15:29:35 by jonas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static int	get_tex_x(t_dda *dda)
+{
+	int	temp;
+
+	temp = (int)(dda->raydirx * TILE_SIZE);
+	if ((dda->side == 0 && dda->raydirx > 0) || (dda->side == 1 && dda->raydirx < 0))
+        temp = TILE_SIZE - temp - 1;
+	return (temp);
+}
+
+static int	get_tex_y(t_dda *dda, int y)
+{
+	int	temp;
+
+	temp = (int)(y * TILE_SIZE / dda->lineheight) & (TILE_SIZE - 1);
+	return (temp);
+}
+
 int get_tex_colour(t_game *game, int y)
 {
-    int tex_x;
-    int tex_y;
-    int colour;
+    int	tex_x;
+    int	tex_y;
+    int	colour;
 
 	if (!game->no_addr)
 		return (COLOUR_DEFAULT);
-    // Calcula tex_x com base na direção do raio
-    tex_x = (int)(game->dda.raydirx * TILE_SIZE);
-    if ((game->dda.side == 0 && game->dda.raydirx > 0) || (game->dda.side == 1 && game->dda.raydirx < 0))
-        tex_x = TILE_SIZE - tex_x - 1;
-
-    // Calcula tex_y com base na altura da linha
-    tex_y = (int)(y * TILE_SIZE / game->dda.lineheight) & (TILE_SIZE - 1); // usando módulo para garantir que tex_y esteja dentro dos limites da textura
-
-    // Acessa a textura corretamente
+	tex_x = get_tex_x(&game->dda);
+    tex_y = get_tex_y(&game->dda, y);
     colour = *(int *)(game->no_addr + tex_y * game->line_length_no + tex_x * (game->bpp_no / 8));
     
-    return colour;
+    return (colour);
 }
