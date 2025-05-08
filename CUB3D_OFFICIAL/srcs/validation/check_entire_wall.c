8eb55ff16e20df7db136cb3ff9435aed462ac382
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-05-08 18:50:40 -0300

    ok

--- CUB3D_OFFICIAL/srcs/validation/check_entire_wall.c
+++ CUB3D_OFFICIAL/srcs/validation/check_entire_wall.c
@@ -6,7 +6,7 @@
 /*   By: fruan-ba <fruan-ba@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 16:05:55 by fruan-ba          #+#    #+#             */
-/*   Updated: 2025/05/03 19:21:44 by fruan-ba         ###   ########.fr       */
+/*   Updated: 2025/05/08 18:50:19 by fruan-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,6 @@ int	check_entire_wall(t_game *game, int index)
 			&& game->true_game_map[index][count] != ' '
 			&& game->true_game_map[index][count] != '\t')
 			return (ft_putendl_fd_0("Error: Map isn't closed", 2));
-		if (game->true_game_map[index][count] == '\t'
-			&& game->true_game_map[index + 1][count] == '0')
-			return (ft_putendl_fd_0("Error: Invalid tab!", 2));
 		count++;
 	}
 	return (1);
